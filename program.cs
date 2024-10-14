8068bce58296d09d8f20a09a5af1725cec884694
lkssaa <105158753+lkssaa@users.noreply.github.com>
2024-10-14 08:59:08 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -219,260 +219,271 @@ class a
         string h;
         a a1 = new a();
         Random rand = new Random();
-
-        n = Convert.ToInt32(Console.ReadLine());
-        switch (n)
+        Console.WriteLine("task number: ");
+        try
         {
-            case 1:
-                Console.WriteLine("more than two digit integer inclusive: ");
+            n = Convert.ToInt32(Console.ReadLine());
+        
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("more than two digit integer inclusive: ");
 
-                n = Convert.ToInt32(Console.ReadLine());
+                    n = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("sum of last two numbers: " + a1.sumLastNums(n).ToString());
-                break;
+                    Console.WriteLine("sum of last two numbers: " + a1.sumLastNums(n).ToString());
+                    break;
 
 
 
-            case 2:
-                Console.WriteLine("any integer: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("positive: " + a1.isPositive(n).ToString());
-                break;
+                case 2:
+                    Console.WriteLine("any integer: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("positive: " + a1.isPositive(n).ToString());
+                    break;
 
 
 
-            case 3:
-                char s;
-                Console.WriteLine("any character: ");
-                s = Console.ReadLine()[0];
-                Console.WriteLine("upper case: " + a1.isUpperCase(s).ToString());
-                break;
+                case 3:
+                    char s;
+                    Console.WriteLine("any character: ");
+                    s = Console.ReadLine()[0];
+                    Console.WriteLine("upper case: " + a1.isUpperCase(s).ToString());
+                    break;
 
 
 
-            case 4:
-                Console.WriteLine("any integer 1: ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("any integer 2: ");
-                y = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("can be divided: " + a1.isDivisor(x, y).ToString());
-                break;
+                case 4:
+                    Console.WriteLine("any integer 1: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("any integer 2: ");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("can be divided: " + a1.isDivisor(x, y).ToString());
+                    break;
 
 
 
-            case 5:
-                Console.WriteLine("sum of the last digits: ");
-                int[] ns = new int[5];
+                case 5:
+                    Console.WriteLine("sum of the last digits: ");
+                    int[] ns = new int[5];
 
-                for (int i = 0; i < 5; i++)
-                {
-                    ns[i] = rand.Next(100);
-                    Console.Write($"{ns[i]} ");
-                    if (i != 4) Console.Write("+ ");
-                }
-                int t = a1.lastNumSum(ns[0], ns[1]);
-                for (int i = 2; i < 5; i++)
-                {
-                    t = a1.lastNumSum(ns[i], t);
-                }
-                Console.WriteLine($"= {t}");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        ns[i] = rand.Next(100);
+                        Console.Write($"{ns[i]} ");
+                        if (i != 4) Console.Write("+ ");
+                    }
+                    int t = a1.lastNumSum(ns[0], ns[1]);
+                    for (int i = 2; i < 5; i++)
+                    {
+                        t = a1.lastNumSum(ns[i], t);
+                    }
+                    Console.WriteLine($"= {t}");
 
-                break;
+                    break;
 
 
 
-            case 6:
-                Console.WriteLine("any integer 1: ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("any integer 2: ");
-                y = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("divide result: " + a1.safeDiv(x, y).ToString());
-                break;
+                case 6:
+                    Console.WriteLine("any integer 1: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("any integer 2: ");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("divide result: " + a1.safeDiv(x, y).ToString());
+                    break;
 
 
 
-            case 7:
-                Console.WriteLine("any integer 1: ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("any integer 2: ");
-                y = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("decision result: " + a1.makeDecision(x, y));
-                break;
+                case 7:
+                    Console.WriteLine("any integer 1: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("any integer 2: ");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("decision result: " + a1.makeDecision(x, y));
+                    break;
 
 
 
-            case 8:
-                Console.WriteLine("any integer 1: ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("any integer 2: ");
-                y = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("any integer 3: ");
-                z = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("is sum: " + a1.sum3(x, y, z).ToString());
-                break;
+                case 8:
+                    Console.WriteLine("any integer 1: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("any integer 2: ");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("any integer 3: ");
+                    z = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("is sum: " + a1.sum3(x, y, z).ToString());
+                    break;
 
 
 
-            case 9:
-                Console.WriteLine("any integer: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("age: " + a1.age(n));
-                break;
+                case 9:
+                    Console.WriteLine("any integer: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("age: " + a1.age(n));
+                    break;
 
 
 
-            case 10:
-                Console.WriteLine("any russian day of the week with a small letter: ");
-                h = Console.ReadLine();
-                a1.printDays(h);
-                break;
+                case 10:
+                    Console.WriteLine("any russian day of the week with a small letter: ");
+                    h = Console.ReadLine();
+                    a1.printDays(h);
+                    break;
 
 
 
-            case 11:
-                Console.WriteLine("any integer: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("previous numbers:" + a1.reverseListNums(n));
-                break;
+                case 11:
+                    Console.WriteLine("any integer: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("previous numbers:" + a1.reverseListNums(n));
+                    break;
 
 
-
-            case 12:
-                Console.WriteLine("any integer 1(base): ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("any integer 2(power): ");
-                y = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("result: " + a1.pow(x, y).ToString());
-                break;
-
-
-
-            case 13:
-                Console.WriteLine("any integer: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                a1.leftTriangle(n);
-                break;
-
-
-
-            case 14:
-                Console.WriteLine("any integer: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("equal numbers: " + a1.equalNum(n).ToString());
-                break;
-
-
-
-            case 15:
-                a1.guessGame();
-                break;
-
-
-
-            case 16:
-                Console.WriteLine("array length: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                ns1 = new int[n];
-                rand = new Random();
-                for (int i = 0; i < n; i++)
-                {
-                    ns1[i] = rand.Next(100);
-                    Console.Write($"{ns1[i]} ");
-                    if (i != n - 1) Console.Write(", ");
-                }
-                Console.WriteLine("\nnumber to find: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("last index of this number: " + a1.findLast(ns1, n).ToString());
-                break;
-
-
-
-            case 17:
-                Console.WriteLine("array length: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                ns1 = new int[n];
-                rand = new Random();
-                for (int i = 0; i < n; i++)
-                {
-                    ns1[i] = rand.Next(100);
-                    Console.Write($"{ns1[i]} ");
-                    if (i != n - 1) Console.Write(", ");
-                }
-                Console.WriteLine("number to replace: ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("position to replace: ");
-                y = Convert.ToInt32(Console.ReadLine());
-
-                OutputArray(a1.add(ns1, x, y - 1));
-                break;
-
-
-
-            case 18:
-                Console.WriteLine("array length: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                ns1 = new int[n];
-                rand = new Random();
-                for (int i = 0; i < n; i++)
-                {
-                    ns1[i] = rand.Next(100);
-                    Console.Write($"{ns1[i]} ");
-                    if (i != n - 1) Console.Write(", ");
-                }
-                Console.Write("\n");
-                a1.reverse(ns1);
-                OutputArray(ns1);
-                break;
-
-
-
-            case 19:
-                Console.WriteLine("array 1 length: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                ns1 = new int[n];
-                rand = new Random();
-                for (int i = 0; i < n; i++)
-                {
-                    ns1[i] = rand.Next(100);
-                    Console.Write($"{ns1[i]} ");
-                    if (i != n - 1) Console.Write(", ");
-                }
-                Console.Write("\n");
-                Console.WriteLine("array 2 length: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                ns2 = new int[n];
-                rand = new Random();
-                for (int i = 0; i < n; i++)
-                {
-                    ns2[i] = rand.Next(100);
-                    Console.Write($"{ns2[i]} ");
-                    if (i != n - 1) Console.Write(", ");
-                }
-                Console.Write("\n");
-
-                OutputArray(a1.concat(ns1, ns2));
-                break;
-
-
-
-            case 20:
-                Console.WriteLine("array length: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                ns1 = new int[n];
-                rand = new Random();
-                for (int i = 0; i < n; i++)
-                {
-                    ns1[i] = rand.Next(100);
-                    Console.Write($"{ns1[i]} ");
-                    if (i != n - 1) Console.Write(", ");
-                }
-                Console.Write("\n");
-                OutputArray(a1.deleteNegative(ns1));
-                break;
 
+                case 12:
+                    Console.WriteLine("any integer 1(base): ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("any integer 2(power): ");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("result: " + a1.pow(x, y).ToString());
+                    break;
+
+
+
+                case 13:
+                    Console.WriteLine("any integer: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    a1.leftTriangle(n);
+                    break;
+
+
+
+                case 14:
+                    Console.WriteLine("any integer: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("equal numbers: " + a1.equalNum(n).ToString());
+                    break;
+
+
+
+                case 15:
+                    a1.guessGame();
+                    break;
+
+
+
+                case 16:
+                    Console.WriteLine("array length: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    ns1 = new int[n];
+                    rand = new Random();
+                    for (int i = 0; i < n; i++)
+                    {
+                        ns1[i] = rand.Next(100);
+                        Console.Write($"{ns1[i]} ");
+                        if (i != n - 1) Console.Write(", ");
+                    }
+                    Console.WriteLine("\nnumber to find: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("last index of this number: " + a1.findLast(ns1, n).ToString());
+                    break;
+
+
+
+                case 17:
+                    Console.WriteLine("array length: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    ns1 = new int[n];
+                    rand = new Random();
+                    for (int i = 0; i < n; i++)
+                    {
+                        ns1[i] = rand.Next(100);
+                        Console.Write($"{ns1[i]} ");
+                        if (i != n - 1) Console.Write(", ");
+                    }
+                    Console.WriteLine("number to replace: ");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("position to replace: ");
+                    y = Convert.ToInt32(Console.ReadLine());
+
+                    OutputArray(a1.add(ns1, x, y - 1));
+                    break;
+
+
+
+                case 18:
+                    Console.WriteLine("array length: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    ns1 = new int[n];
+                    rand = new Random();
+                    for (int i = 0; i < n; i++)
+                    {
+                        ns1[i] = rand.Next(100);
+                        Console.Write($"{ns1[i]} ");
+                        if (i != n - 1) Console.Write(", ");
+                    }
+                    Console.Write("\n");
+                    a1.reverse(ns1);
+                    OutputArray(ns1);
+                    break;
+
+
+
+                case 19:
+                    Console.WriteLine("array 1 length: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    ns1 = new int[n];
+                    rand = new Random();
+                    for (int i = 0; i < n; i++)
+                    {
+                        ns1[i] = rand.Next(100);
+                        Console.Write($"{ns1[i]} ");
+                        if (i != n - 1) Console.Write(", ");
+                    }
+                    Console.Write("\n");
+                    Console.WriteLine("array 2 length: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    ns2 = new int[n];
+                    rand = new Random();
+                    for (int i = 0; i < n; i++)
+                    {
+                        ns2[i] = rand.Next(100);
+                        Console.Write($"{ns2[i]} ");
+                        if (i != n - 1) Console.Write(", ");
+                    }
+                    Console.Write("\n");
+
+                    OutputArray(a1.concat(ns1, ns2));
+                    break;
+
+
+
+                case 20:
+                    Console.WriteLine("array length: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    ns1 = new int[n];
+                    rand = new Random();
+                    for (int i = 0; i < n; i++)
+                    {
+                        ns1[i] = rand.Next(100);
+                        Console.Write($"{ns1[i]} ");
+                        if (i != n - 1) Console.Write(", ");
+                    }
+                    Console.Write("\n");
+                    OutputArray(a1.deleteNegative(ns1));
+                    break;
+                default:
+                    Console.WriteLine("no such task");
+                    break;
+            }
         }
-
-
-
+        catch (System.OverflowException e)
+        {
+            Console.WriteLine("incorrect array length");
+        }
+        catch (System.FormatException e)
+        {
+            Console.WriteLine("incorrect input");
+        }
     }
 }
